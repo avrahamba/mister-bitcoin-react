99696c264fa2876c104c39eb20bc09e5396b892b
Avraham <b.a.avraham@gmail.com>
2020-04-26 04:42:07 +0300

    Updates

---
+++ precache-manifest.d10b357450e668d38831d68c49274c5a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbdbe017a1dd13ec04b7d0bf93eb896c",
+    "url": "/mister-bitcoin-react/index.html"
+  },
+  {
+    "revision": "34d26fe5e6bc42a4f05b",
+    "url": "/mister-bitcoin-react/static/css/main.aa8d40e3.chunk.css"
+  },
+  {
+    "revision": "2402df2ec0b949350899",
+    "url": "/mister-bitcoin-react/static/js/2.b4cd6948.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mister-bitcoin-react/static/js/2.b4cd6948.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34d26fe5e6bc42a4f05b",
+    "url": "/mister-bitcoin-react/static/js/main.f3ffe79f.chunk.js"
+  },
+  {
+    "revision": "24e2441a2839f8726351",
+    "url": "/mister-bitcoin-react/static/js/runtime-main.32249ce4.js"
+  },
+  {
+    "revision": "d2e691bc4a2b696929172cb3d22ce8ba",
+    "url": "/mister-bitcoin-react/static/media/Nunito-Regular.d2e691bc.ttf"
+  },
+  {
+    "revision": "da05397dfa7d774a1fea86947adc0559",
+    "url": "/mister-bitcoin-react/static/media/add-contact.da05397d.svg"
+  },
+  {
+    "revision": "7a10c171fdeaf582c8100f5b5fcd054a",
+    "url": "/mister-bitcoin-react/static/media/back.7a10c171.svg"
+  },
+  {
+    "revision": "56e5ff88da9ad1dcc2d5299425022e85",
+    "url": "/mister-bitcoin-react/static/media/bitcoin.56e5ff88.svg"
+  },
+  {
+    "revision": "570dbdbd84e40f66b5564887c2cba3e1",
+    "url": "/mister-bitcoin-react/static/media/edit-contact.570dbdbd.svg"
+  },
+  {
+    "revision": "8eacb5a5fea53ae15b4b0cbb6e628345",
+    "url": "/mister-bitcoin-react/static/media/home.8eacb5a5.svg"
+  },
+  {
+    "revision": "de02feb67800958d0ab472d0e5c2dfaf",
+    "url": "/mister-bitcoin-react/static/media/members.de02feb6.svg"
+  }
+]);
